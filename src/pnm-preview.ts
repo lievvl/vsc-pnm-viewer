cb5964a62790802efe7c39a40edbaf308b8858ae
lievvl <37303364+lievvl@users.noreply.github.com>
2022-11-19 23:30:28 +0300

    feat: done parsing

--- src/pnm-preview.ts
+++ src/pnm-preview.ts
@@ -47,11 +47,85 @@ class PnmDocument
     }
     
     public get getParsedImage() {
-        return parse.parseByteFormat(this.getDocumentData);
+        return this.parse();
     }
-    
+
     dispose(): void {
         super.dispose();
+    }
+
+    private parse() {
+        let currentByte = 0,
+            len = this._image.length,
+            l = 0;
+        let magic = String.fromCharCode(this._image[currentByte]);
+        currentByte += 1;
+        let number = String.fromCharCode(this._image[currentByte]);
+        currentByte += 1;
+        if (magic + number !== "P6") {
+            throw new Error("Wrong file magic number");
+        }
+        
+        let widthString = "";
+        currentByte += 1;
+        while (true)
+        {
+            let inp = String.fromCharCode(this._image[currentByte]);
+            currentByte += 1;
+            if (!('0' <= inp && inp <= '9'))
+            {
+                break;
+            }
+            widthString += inp;
+        }
+        let width = Number.parseInt(widthString);
+
+        let heightString = "";
+        while (true)
+        {
+            let inp = String.fromCharCode(this._image[currentByte]);
+            currentByte += 1;
+            if (!('0' <= inp && inp <= '9'))
+            {
+                break;
+            }
+            heightString += inp;
+        }
+        let height = Number.parseInt(heightString);
+
+        let maxValString = "";
+        while (true)
+        {
+            let inp = String.fromCharCode(this._image[currentByte]);
+            currentByte += 1;
+            if (!('0' <= inp && inp <= '9'))
+            {
+                break;
+            }
+            maxValString += inp;
+        }
+        let maxVal = Number.parseInt(maxValString);
+
+        let colorData: { r: number; g: number; b: number }[][] = [];
+        const totalPixels = width * height;
+        for (let y = 0; y < height; y++) {
+            for (let x = 0; x < width; x++) {
+                let r = this._image[currentByte];
+                currentByte += 1;
+                let g = this._image[currentByte];
+                currentByte += 1;
+                let b = this._image[currentByte];
+                currentByte += 1;
+                colorData[x][y] = {
+                    r: r,
+                    g: g,
+                    b: b
+                };
+            }
+        }
+
+        return {width, height, colorData};
+
     }
 
 }
